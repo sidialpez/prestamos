707c4be78635efbec41ede8a0e5ddcb7cf95c4b3
Sidia <sidialpez@gmail.com>
2025-05-02 22:22:00 -0600

    Versionamiento del proyecto

---
+++ src/main/java/com/pruebatecnica/prestamobancario/PrestamobancarioApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.prestamobancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamobancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamobancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebatecnica/prestamobancario/PrestamobancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.prestamobancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamobancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
